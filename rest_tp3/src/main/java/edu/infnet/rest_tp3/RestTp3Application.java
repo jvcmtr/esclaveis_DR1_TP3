e0584d55347f4687372e6a4a216c5faf29cffd6e
jvcmtr <jvictorcmtr@gmail.com>
2025-09-13 20:37:02 -0300

    Init project using spring initializr

---
+++ rest_tp3/src/main/java/edu/infnet/rest_tp3/RestTp3Application.java
@@ -0,0 +1,13 @@
+package edu.infnet.rest_tp3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTp3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestTp3Application.class, args);
+	}
+
+}
---
+++ rest_tp3/src/test/java/edu/infnet/rest_tp3/RestTp3ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.infnet.rest_tp3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestTp3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
